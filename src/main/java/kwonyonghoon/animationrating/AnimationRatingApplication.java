dda9a337500daaf0169c2379e5fc02fbb293b800
kwonyonghoon <biggrace681@gmail.com>
2025-03-10 17:33:50 +0900

    Initial commit

---
+++ src/main/java/kwonyonghoon/animationrating/AnimationRatingApplication.java
@@ -0,0 +1,11 @@
+package kwonyonghoon.animationrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimationRatingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimationRatingApplication.class, args);
+    }
+}
